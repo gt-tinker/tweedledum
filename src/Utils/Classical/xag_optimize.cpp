c323348eacb1b32b6ee7bb39457a404a5e750b76
Austin Adams <git@austinjadams.com>
2024-03-26 18:42:42 -0400

    Remove `#pragma once` from .cpp file

--- src/Utils/Classical/xag_optimize.cpp
+++ src/Utils/Classical/xag_optimize.cpp
@@ -2,7 +2,6 @@
 | Part of Tweedledum Project.  This file is distributed under the MIT License.
 | See accompanying file /LICENSE for details.
 *-----------------------------------------------------------------------------*/
-#pragma once
 
 #include "tweedledum/Utils/Classical/xag_optimize.h"
 
