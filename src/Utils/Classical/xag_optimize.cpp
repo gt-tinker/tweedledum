0f18e657c32c5168a53bc06977d4267496ea696f
Austin Adams <git@austinjadams.com>
2024-03-26 17:44:38 -0400

    Move xag_optimize() into a .cpp file
    This makes it within humanity's grasp for a dependency to write
    `#include <tweedledum/xag_optimize.h>` on Windows. Otherwise, that
    `#include` pulls in many header files for SAT solver libraries with
    questionable Windows compatibility into innocent pure-hearted codebases
    who have Tweedledum as a dependency.
    
    The specific issue I was running into is the following chain of
    includes:
    
        my-project/innocent-header-file.hpp
        tweedledum/Utils/Classical/xag_optimize.h
        mockturtle/algorithms/xag_optimization.hpp
        bill/sat/interface/common.hpp
        bill/sat/solver/glucose.hpp
    
    The last of which contains `__sync_bool_compare_and_swap`, a GCC
    intrinsic. Somehow this compiles inside the tweedledum source tree, but
    not inside of mine. (The commit lsils/bill@02cf4092e6d shows that other
    people have built bill on Windows, but I do not know how.)
    
    This commit also avoids the need for dependencies to add the vendored
    SAT solver libraries to their include path.

---
+++ src/Utils/Classical/xag_optimize.cpp
@@ -0,0 +1,53 @@
+/*------------------------------------------------------------------------------
+| Part of Tweedledum Project.  This file is distributed under the MIT License.
+| See accompanying file /LICENSE for details.
+*-----------------------------------------------------------------------------*/
+#pragma once
+
+#include "tweedledum/Utils/Classical/xag_optimize.h"
+
+#include <mockturtle/algorithms/cleanup.hpp>
+#include <mockturtle/algorithms/cut_rewriting.hpp>
+#include <mockturtle/algorithms/node_resynthesis/bidecomposition.hpp>
+#include <mockturtle/algorithms/node_resynthesis/xag_minmc2.hpp>
+#include <mockturtle/algorithms/refactoring.hpp>
+#include <mockturtle/algorithms/xag_optimization.hpp>
+#include <mockturtle/networks/xag.hpp>
+#include <mockturtle/properties/mccost.hpp>
+
+namespace tweedledum {
+
+void xag_optimize(mockturtle::xag_network& xag)
+{
+    using namespace mockturtle;
+    // Parameters: CUT rewriting using database
+    future::xag_minmc_resynthesis resyn;
+    cut_rewriting_params cut_rwrt_cfg;
+    cut_rwrt_cfg.cut_enumeration_ps.cut_size = 5;
+
+    // Parameters: Refactoring
+    mockturtle::bidecomposition_resynthesis<xag_network> bi_resyn;
+    refactoring_params r_params;
+    r_params.allow_zero_gain = true;
+    // r_params.use_dont_cares = tru;
+
+    xag = xag_constant_fanin_optimization(xag);
+    xag = cleanup_dangling(xag);
+    xag = xag_dont_cares_optimization(xag);
+    xag = cleanup_dangling(xag);
+
+    refactoring(xag, bi_resyn, r_params, nullptr, free_xor_cost<xag_network>());
+    // refactoring(xag, bi_resyn);
+    xag = cleanup_dangling(xag);
+
+    cut_rewriting_with_compatibility_graph(
+      xag, resyn, cut_rwrt_cfg, nullptr, mc_cost<xag_network>());
+    xag = cleanup_dangling(xag);
+
+    xag = xag_constant_fanin_optimization(xag);
+    xag = cleanup_dangling(xag);
+    xag = xag_dont_cares_optimization(xag);
+    xag = cleanup_dangling(xag);
+}
+
+} // namespace tweedledum
